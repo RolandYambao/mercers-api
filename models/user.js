ca5486fde9c2a20815f6c09f631ebdbd2e373a90
RolandYambao <rolandyambao@gmail.com>
2022-01-03 16:44:22 -0800

    Updated

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     userName: String,
+    profilePic: String,
     email: String,
     address: String,
     phone: Number,
